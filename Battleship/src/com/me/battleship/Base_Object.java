c38665b4020d0619b03fffa7f3903b15df6700c1
David Zhou <davzhou13@gmail.com>
2013-10-24 22:27:18 -0500

    added another class

---
+++ Battleship/src/com/me/battleship/Base_Object.java
@@ -0,0 +1,10 @@
+package com.me.battleship;
+
+public class Base_Object {
+    int column, row;
+
+    Base_Object(int x, int y) {
+        column = y;
+        row = x;
+    }
+}
---
+++ Battleship/src/com/me/battleship/Torpedo.java
@@ -0,0 +1,9 @@
+package com.me.battleship;
+
+public class Torpedo extends Base_Object {
+
+    Torpedo(int x, int y) {
+        super(x, y);
+
+    }
+}
