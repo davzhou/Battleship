a5775cdc1bfeb6cee50745c9c08dd96880ba6b09
David Zhou <davzhou13@gmail.com>
2013-10-24 21:32:44 -0500

    made some progress on core classes

---
+++ Battleship/src/com/me/battleship/Globals.java
@@ -0,0 +1,16 @@
+package com.me.battleship;
+
+public class Globals {
+
+    public static final int EMPTY = 0;
+    public static final int FRIGATE = 1;
+    public static final int CRUISER = 2;
+    public static final int SUBMARINE = 3;
+    public static final int BATTLESHIP = 4;
+    public static final int CARRIER = 5;
+    public static final int TARGETED = -6;
+
+    public static enum Orientation {
+        VERTICAL, HORIZONTAL
+    }
+}
--- Battleship/src/com/me/battleship/Ship.java
+++ Battleship/src/com/me/battleship/Ship.java
@@ -2,4 +2,14 @@ package com.me.battleship;
 
 public class Ship {
 
+    int column, row, shipClass;
+    Globals.Orientation orientation;
+
+    Ship(int x, int y, int c, Globals.Orientation o){
+        row=x;
+        column=y;
+        shipClass=c;
+        orientation=o;
+    }
+
 }
